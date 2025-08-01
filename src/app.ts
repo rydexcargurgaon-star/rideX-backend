6b818305a1e34752a2034dd47e8d08e789dc439d
sushanthumiitech <sushantsrk2001@gmail.com>
2025-08-02 00:30:13 +0530

    added the auto hit api

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import chalk from "chalk";
 import express from "express";
 import routes from "./routes/index.js";
 import cors from "cors";
+import axios from "axios";
 
 const port = 8080;
 const app = express();
@@ -9,6 +10,23 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use("/", routes);
+
+app.get("/health", (req, res) => {
+  res.status(200).send("Healthy");
+});
+
+const hitAPI = async () => {
+  try {
+    const response = await axios.get(
+      "https://ridex-backend-2hu0.onrender.com/health"
+    );
+    console.log("API Response:", response.data);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+setInterval(hitAPI, 10000);
 
 app.listen(port, () => {
   console.log(
